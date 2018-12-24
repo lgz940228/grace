dacd6ed2ceb1cde7fed807c1b7c02b0f6308c414
刘国章 <guozhang.liu>
2018-12-24 14:58:19 +0800

    feat:dept provider

--- grace-api/src/main/java/com/lgz/grace/api/entities/Dept.java
+++ grace-api/src/main/java/com/lgz/grace/api/entities/Dept.java
@@ -1,12 +1,18 @@
 package com.lgz.grace.api.entities;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
 
 /**
  * Created by lgz on 2018/11/23.
  */
 @Data
-public class Dept {
+@NoArgsConstructor
+@Accessors(chain = true)
+public class Dept implements Serializable{
     private Integer deptno;
     private String dname;
     private String dbSource;
