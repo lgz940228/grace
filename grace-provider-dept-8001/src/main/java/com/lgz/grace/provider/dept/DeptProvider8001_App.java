4130a900f284a136ec334aa3e4c16cd9984e469e
刘国章 <guozhang.liu>
2018-12-24 17:54:00 +0800

    feat:eureka server

--- grace-provider-dept-8001/src/main/java/com/lgz/grace/provider/dept/DeptProvider8001_App.java
+++ grace-provider-dept-8001/src/main/java/com/lgz/grace/provider/dept/DeptProvider8001_App.java
@@ -2,11 +2,13 @@ package com.lgz.grace.provider.dept;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Created by lgz on 2018/11/23.
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class DeptProvider8001_App {
     public static void main(String[] args) {
         SpringApplication.run(DeptProvider8001_App.class, args);
