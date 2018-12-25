4f5d6df26437c93dc32516029ffaabc1c2852115
lgz <lgz940228@163.com>
2018-12-25 21:52:27 +0800

    feat:添加ReflectUtil

---
+++ grace-api/src/main/java/com/lgz/grace/api/utils/UUIDUtil.java
@@ -0,0 +1,19 @@
+package com.lgz.grace.api.utils;
+
+import java.util.UUID;
+
+public class UUIDUtil {
+    public static String newUuid() {
+        return UUID.randomUUID().toString().replaceAll("-", "");
+    }
+
+    /*public static void main(String args[]){
+        try {
+            throw new RuntimeException("123");
+        }catch (Exception e){
+            throw new RuntimeException("321",e);
+        }finally {
+            System.out.println("finally");
+        }
+    }*/
+}
