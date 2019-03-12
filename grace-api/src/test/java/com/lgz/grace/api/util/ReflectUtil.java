22601c7713c3069cdcc9a26a2d1513c5e7396514
刘国章 <guozhang.liu>
2019-03-12 14:38:08 +0800

    feat:ReflectUtil  getset

--- grace-api/src/test/java/com/lgz/grace/api/util/ReflectUtil.java
+++ grace-api/src/test/java/com/lgz/grace/api/util/ReflectUtil.java
@@ -1,9 +1,13 @@
 package com.lgz.grace.api.util;
 
+import com.lgz.grace.api.Person;
 import com.lgz.grace.api.annotation.Excel;
 import com.lgz.grace.api.entities.Record;
 import org.junit.Test;
 
+import java.beans.BeanInfo;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
@@ -30,6 +34,33 @@ public class ReflectUtil {
             clazz = clazz.getSuperclass();
         } while (clazz != Object.class && clazz != null);
         return list.toArray(fields);
+    }
+
+    @Test
+    public void getset()throws Exception{
+        Person person = new Person();
+        person.setName("小红");
+        person.setMobile("15222222222");
+        person.setSex("女");
+        BeanInfo beanInfo = Introspector.getBeanInfo(person.getClass(),Object.class);
+        PropertyDescriptor[] propertyDescriptors = beanInfo.getPropertyDescriptors();
+        for (PropertyDescriptor pd : propertyDescriptors){
+            if(pd.getPropertyType().isArray())  //getPropertyType得到属性类型。
+            {
+                //getReadMethod()得到此属性的get方法----Method对象，然后用invoke调用这个方法
+                String[] result=(String[]) pd.getReadMethod().invoke(person, null);
+                System.out.println(pd.getName()+":");//getName得到属性名字
+                for (int j = 0; j < result.length; j++) {
+                    System.out.println(result[j]);
+                }
+            }
+            else{
+                if("id".equals(pd.getName())){
+                    pd.getWriteMethod().invoke(person,"123");
+                }
+                System.out.println(pd.getName()+":"+pd.getReadMethod().invoke(person, null));
+            }
+        }
     }
 
     @Test
